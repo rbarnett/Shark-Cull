733da1d1a930ae6f8b5d86f3106e8a8f7503a575
Rob Barnett <robertpbarnett@gmail.com>
2014-03-03 14:30:20 +0800

    Fix relative path problem when deploying

--- config.rb
+++ config.rb
@@ -10,9 +10,17 @@ end
 
 configure :build do
   activate :minify_css
+  activate :relative_assets
+  set :relative_links, true
 # activate :minify_javascript
 end
 
 ready do
   sprockets.append_path '../src'
+end
+
+helpers do
+  def asset(path)
+  	image_path(path).gsub(/^..\//, '')
+  end
 end
